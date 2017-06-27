e88a6889c6f3d29420eef9be45f39a060775523a
RainHawk616 <RainHawk616@users.noreply.github.com>
2017-06-27 12:50:06 -0500

    Handle non-object arrays

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -35,7 +35,8 @@ $(document).ready(function () {
                     const elemType = getConstructorName(elem);
 
                     str += ` * @property {${elemType}[]} ${propertyName}\n`;
-                    str += recursiveTyping(elem, `${propertyName}[]`)
+                    if( elemType === 'Object')
+                        str += recursiveTyping(elem, `${propertyName}[]`)
                 }
                 // else if(variable) {
                 //     str += ` * @property {${type}} ${propertyName} - ${variable}\n`;
@@ -52,9 +53,10 @@ $(document).ready(function () {
     $("#convert").click(function () {
         const jsDoc = $('#jsdoc');
 
+        let val = $('#json').val().replace(/\r?\n|\r/gm, ``);//.replace(/\s*[^"](\w+)\s*[^"]\s*:/, `"$1":`);
         try {
-            //let val = $('#json').val().replace(/\s*[^"](\w+)\s*[^"]\s*:/, `"$1":`);
-            let obj = JSON.parse($('#json').val());
+            //
+            let obj = JSON.parse(val);
 
             let str = `/**\n`
                 +` * @typedef {Object} newType\n`
@@ -64,6 +66,22 @@ $(document).ready(function () {
             jsDoc.val(str);
 
         } catch (err) {
+            const regex = /Unexpected token (.|\r?\n|\r) in JSON at position (\d+)/gm;
+            const match = regex.exec(err.message);
+
+            if( match && match.length > 0 ) {
+                const character = match[1];
+                const location = parseInt(match[2]);
+                const start = Math.max(0,location-5);
+                const end = Math.min(11, val.length);
+
+                console.log(location)
+                console.log(start);
+                console.log(end);
+
+                err.message += `\r\nNear: ${val.substr(start,end)}`;
+            }
+
             jsDoc.val(err.message);
         }
     });
